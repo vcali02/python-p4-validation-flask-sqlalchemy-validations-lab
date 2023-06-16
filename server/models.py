181c0d5be171ed904f671b3a60ec98d27330ec28
valeria caliguire <caliguireval@gmail.com>
2023-06-16 10:09:52 -0600

    completed core deliverables

--- server/models.py
+++ server/models.py
@@ -15,6 +15,23 @@ class Author(db.Model):
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not  name:
+            raise ValueError('Name field is required.')
+        elif name in names:
+            raise ValueError('Name must be unique.')
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
+
+
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
@@ -30,3 +47,30 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        title = db.session.query(Post.title).all()
+        if not title:
+            raise ValueError('Must contain a title.')
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least  250 characters.')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary cannot exceed 250 characters.')
+        return summary
+
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Category must be Fiction or Non-Fiction.')
+        return category
